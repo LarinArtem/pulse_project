c698b59837f4f1a266e8cf53f7bc8fc8611fc9c3
Artem <you@example.com>
2021-07-09 16:13:08 +0300

    add js file and a couple of scss files

---
+++ src/js/script.js
@@ -0,0 +1,47 @@
+// let number = 21
+// console.log(number);
+// let answer = prompt("Вам есть 18?");
+// console.log(answer);
+// alert(123)
+
+// let answer = confirm("Вам есть 18?", "");
+// if (answer){
+//     console.log('GO')
+// }else{
+//     console.log('STOP')
+// }
+
+// let isChecked = true,
+//     isClose = false;
+
+// console.log(isChecked && isClose);
+
+// console.log(isChecked || isClose);
+
+// if(3*3 == 10){
+//     console.log('true')
+// }else{
+//     console.log("false")
+// }
+
+// const num = 50;
+
+// if(num < 50){
+//     console.log('not correct')
+// }else if(num > 100){
+//     console.log('too much')
+// }else{
+//     console.log('correct')
+// }
+
+// for(let i = 1; i< 8; i++){
+//     console.log(i);
+// }
+
+function logging(a, b, c){
+    console.log(a+b*c)
+}
+
+logging(3,6,9);
+
+logging(10, 23, 16)
