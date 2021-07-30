b7815b74d749775e3339622aa59d7d52e4b9caae
Artem <you@example.com>
2021-07-30 20:08:12 +0300

    add php and js

--- src/js/script.js
+++ src/js/script.js
@@ -103,4 +103,60 @@ $(document).ready(function(){
 
     // toggleSlide('.catalog-item__link');
     // toggleSlide('.catalog-item__back');
+
+     $('[data-modal=consultation]').on('click', function(){
+      $('.overlay,#consultation').fadeIn('slow')
+     });
+     $('.modal__close').on('click',function(){
+      $('.overlay,#concultation, #thanks, #order').fadeOut('slow')
+     });
+     $('.button_mini').each(function(i){
+      $(this).on('click',function(){
+        $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+        $('.overlay, #order').fadeIn('slow');
+      })
+     });
+     function valideForms(form){
+      $(form).validate({
+       rules:{
+         name: "required",
+         phone: "required",
+         email: {
+           required: true,
+           email: true
+         }
+       },
+       messages: {
+        name: "Пожалуйста, введите своё имя",
+        phone: "Пожалуйста, введите свой номер ",
+        email: {
+          required: "Пожалуйста, введите свою почту",
+          email: "Неправильно введен адрес почты"
+        }
+      }
+      });
+    };
+    valideForms('#consultation-form');
+    valideForms('#consultation form');
+    valideForms('#order form'); 
+
+    $('input[name=phone]').mask("+7 (999) 999-9999");
+
+    $('form').submit(function(e) {
+      e.preventDefault();
+      $.ajax({
+          type: "POST",
+          url: "mailer/smart.php",
+          data: $(this).serialize()
+      }).done(function() {
+          $(this).find("input").val("");
+          $('#consultation, #order').fadeOut();
+          $('.overlay, #thanks').fadeIn('slow');
+
+          $('form').trigger('reset');
+      });
+      return false;
+  });
+
+     
 });
