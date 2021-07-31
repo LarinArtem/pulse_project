7160a76cd6f44bb3c2c373ddb3b15e161b01fa3e
Artem <you@example.com>
2021-07-31 20:20:53 +0300

    finish php forms and now it's working add scroll animation and heartbeat animation

--- src/mailer/smart.php
+++ src/mailer/smart.php
@@ -13,12 +13,12 @@ $mail->CharSet = 'utf-8';
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'iamtestingweb834@gmail.com';                 // Наш логин
-$mail->Password = 'Qw321456987';                           // Наш пароль от ящика
+$mail->Username = 'lastchancetowin0@gmail.com';                 // Наш логин
+$mail->Password = 'A321456987';                           // Наш пароль от ящика
 $mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 465;                                    // TCP port to connect to
  
-$mail->setFrom('iamtestingweb834@gmail.com', 'Pulse');   // От кого письмо 
+$mail->setFrom('lastchancetowin0@gmail.com', 'Pulse');   // От кого письмо 
 $mail->addAddress('johogim702@biohorta.com');     // Add a recipient
 //$mail->addAddress('ellen@example.com');               // Name is optional
 //$mail->addReplyTo('info@example.com', 'Information');
